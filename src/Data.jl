a6cc39b93e4e59542fe6107800265085a133ef72
Adam Kowalski <adam.kowalski.work@gmail.com>
2019-01-23 17:33:25 -0800

    label decode

--- src/Data.jl
+++ src/Data.jl
@@ -2,7 +2,7 @@ module Data
 
 using Distributed
 
-import Base: getindex, length
+import Base: getindex, length, iterate
 
 export Mapped, Batched
 
@@ -18,15 +18,25 @@ length(m::Mapped) = length(m.dataset)
 
 struct Batched{D}
     batch_size::Int
+    dim::Int
     dataset::D
 end
 
 function getindex(b::Batched, i::Integer)
     lower = (i - 1) * b.batch_size + 1
     upper = min(lower+b.batch_size-1, length(b.dataset))
-    cat(b.dataset[lower:upper]..., dims=4)
+    cat(b.dataset[lower:upper]..., dims=b.dim)
 end
 
 length(b::Batched) = ceil(Int, length(b.dataset) / b.batch_size)
+
+struct Zipped{A, B}
+    a::A
+    b::B
+end
+
+getindex(z::Zipped, i::Integer) = (z.a[i], z.b[i])
+
+iterate(z::Zipped, state=1) = i > length(z) ? nothing : z[state], state + 1
 
 end
--- src/Kratos.jl
+++ src/Kratos.jl
@@ -9,18 +9,20 @@ import .Image, .Data
 
 skip_lines(io::IOStream, n::Integer) = (foreach(_ -> readline(io), 1:n); io)
 
-read_key_values(io::IOStream) = @>> begin
+read_key_values(io::IOStream, T=Dict{String, String}) = @>> begin
     skip_lines(io, 2)
     eachline()
     map(split)
-    Dict{String, String}()
+    T()
 end
 
 data_dir = "deep-fashion"
 
 eval_partition = open(read_key_values, "$data_dir/eval/list_eval_partition.txt")
-category_cloth = open(read_key_values, "$data_dir/anno/list_category_cloth.txt")
 category_img = open(read_key_values, "$data_dir/anno/list_category_img.txt")
+category_cloth = open("$data_dir/anno/list_category_cloth.txt") do file
+    read_key_values(file, Vector)
+end
 
 function partition_dataset(by::AbstractString)
     images = [f for (f, p) in eval_partition if p == by]
@@ -39,15 +41,28 @@ transform = Image.Compose(
 load_image(filename::AbstractString) =
     transform(Image.load("$data_dir/$filename"))
 
+encode_label(label::AbstractString) = Flux.onehot(parse(Int, label), 1:50)
+
+decode_label(encoded::AbstractVector) =
+    category_cloth[Flux.onecold(encoded, 1:50)][1]
+
+
+crossentropy(ŷ::AbstractArray, y::AbstractArray; weight = 1) =
+  -sum(y .* log.(ŷ) .* weight) * 1 // size(y, 2)
+
 train_images = @>> begin
     train[1]
     Data.Mapped(load_image)
-    Data.Batched(10)
+    Data.Batched(10, 4)
 end
 
-image = train_images[1][:, :, :, 1]
+train_labels = @>> begin
+    train[2]
+    Data.Mapped(encode_label)
+    Data.Batched(10, 2)
+end
 
-Image.ToImage(image)
+train_dataset = Data.Zipped(train_images, train_labels)
 
 model = Chain(
     Conv((3, 3), 3=>8, stride=(2, 2)), BatchNorm(8, relu),
@@ -63,13 +78,28 @@ model = Chain(
     MaxPool((2, 2)),
 
     x -> reshape(x, :, size(x, 4)),
-    
+
     Dense(7200, 4000), BatchNorm(4000, relu), Dropout(0.5),
     Dense(4000, 50),
     softmax) |> gpu
 
-model(gpu(train_images[1]))
+(images, labels), state = iterate(train_dataset, state)
 
+predictions = model(gpu(images))
+
+image = images[:, :, :, 4]
+
+label = labels[:, 4]
+
+prediction = predictions[:, 4]
+
+Image.ToImage(image)
+
+decode_label(label)
+
+decode_label(prediction)
+
+train[2][20]
 
 
 end
