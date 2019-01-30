c0debefd2487d66d4cf98bd39bd6b6f3902ff00e
Adam Kowalski <adam.kowalski.work@gmail.com>
2019-01-30 13:59:30 -0800

    data augmentation

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 # %% imports
 import tensorflow as tf
+from tensorflow.data import Dataset
 import pandas as pd
 import matplotlib.pyplot as plt
 import multiprocessing
 import math
+from functools import partial
 
 
 # %% flags
@@ -34,38 +36,41 @@ all_data = eval_partition.merge(category_img, on='image_name')
 
 
 # %% load image
-def load_image(filename):
+def load_image(training, filename):
     image = tf.io.read_file(FLAGS.data_dir + filename)
     image = tf.image.decode_jpeg(image)
     image = tf.image.resize_image_with_crop_or_pad(
         image, FLAGS.height, FLAGS.width)
+
+    if training:
+        image = tf.image.random_flip_left_right(image)
+
     return tf.image.per_image_standardization(image)
 
 
 # %% preprocess data
-def preprocess_data(filename, label):
+def preprocess_data(training, filename, label):
     label = tf.one_hot(label, FLAGS.classes)
-    return load_image(filename), label
+    return load_image(training, filename), label
 
 
 # %% dataset
-def dataset(partition):
+def dataset(partition, training=False):
     data = all_data[all_data['evaluation_status'] == partition]
     data = data.sample(frac=1).reset_index(drop=True)
 
     images = data['image_name'].values
     labels = data['category_label'].values
-    d = (tf.data.Dataset
+    d = (Dataset
          .from_tensor_slices((images, labels))
-         .map(preprocess_data, num_parallel_calls=FLAGS.num_cpus)
+         .map(partial(preprocess_data, training), num_parallel_calls=FLAGS.num_cpus)
          .batch(FLAGS.batch_size)
          .prefetch(FLAGS.prefetch_size)
          .repeat())
 
     return d, len(data)
 
-
-train_dataset, train_length = dataset('train')
+train_dataset, train_length = dataset('train', training=True)
 val_dataset, val_length = dataset('val')
 test_dataset, test_length = dataset('test')
 
@@ -82,6 +87,7 @@ for layer in base_model.layers[16:]:
 
 # %% model
 model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(300, 300, 3)),
     *base_model.layers,
     tf.keras.layers.Dense(1024, activation=tf.keras.activations.relu),
     tf.keras.layers.Dense(FLAGS.classes, activation=tf.keras.activations.softmax),
@@ -92,7 +98,9 @@ model = tf.keras.Sequential([
 model.compile(
     optimizer=tf.keras.optimizers.Adam(),
     loss=tf.keras.losses.categorical_crossentropy,
-    metrics=[tf.keras.metrics.categorical_accuracy])
+    metrics=[
+        tf.keras.metrics.categorical_accuracy,
+        tf.keras.metrics.top_k_categorical_accuracy])
 
 
 # %% fit model
@@ -105,3 +113,7 @@ model.fit(train_dataset, epochs=3,
         tf.keras.callbacks.ModelCheckpoint(
             'checkpoints/model-{epoch:02d}.hdf5', verbose=1)
     ])
+
+# %% evaluate model
+model = tf.keras.models.load_model('checkpoints/model-03.hdf5')
+model.evaluate(test_dataset, steps=math.ceil(test_length / FLAGS.batch_size))
