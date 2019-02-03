0821b1391ec3e18a04b5cdb98f11071a25555edd
Jordan Le <jor25@augusta.cs.pdx.edu>
2019-02-02 18:33:17 -0800

    Updated Evaluate.py to run on chosen.txt - now to graph.

--- model_trial_01/Evaluate.py
+++ model_trial_01/Evaluate.py
@@ -7,16 +7,29 @@ import numpy as np
 
 # Function to normalize images (convert to black and white and reshape)
 def prepare(filepath):
-    IMG_SIZE = 35  # 35 x 35 image
-    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+   IMG_SIZE = 35  # 35 x 35 image
+   img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+   new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+   return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
+
+# Take top 5 predictions if applicable
+def top_n_predictions(one_hot, image, CATEGORIES, n):
+   temp = one_hot
+   for i in range(n): # for top 5 predictions
+      pred_index = np.argmax(temp[0])
+      if (temp[0][pred_index]) <= 0:
+         break
+      else:
+         temp[0][pred_index] = 0.0
+      print("{} prediction({}) as {}".format(image, i, CATEGORIES[pred_index]))
+   print("------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
-   IMG_SIZE = 35
-   DATADIR = '/u/jor25/Capstone/Good_stuff/Verify_Images/'
+   n = 5    # Top n predictions
+   file_name = 'chosen.txt'
+   DATADIR ='/stash/kratos/deep-fashion/category-attribute/' #'/u/jor25/Capstone/Good_stuff/Verify_Images/'
    model = tf.keras.models.load_model("../../Good_stuff/category.model")
 
 
@@ -31,10 +44,15 @@ if __name__ == "__main__":
    "Dress", "Jumpsuit", "Kaftan", "Kimono", "Nightdress",
    "Onesie", "Robe", "Romper", "Shirtdress", "Sundress"]
 
-   path = DATADIR #os.path.join(DATADIR,category)  # create path to dogs and cats
-   for img in os.listdir(path):  # iterate over each image per dogs and cats
-      filepath = os.path.join(path,img)
-      prediction = model.predict([prepare(filepath)]) # gives back a one hot encoding
-      pred_index = np.argmax(prediction)
-      #print("{} = {}. {} predicted as {}".format(img, prediction, img, CATEGORIES[pred_index]))
-      print("{} predicted as {}".format(img, CATEGORIES[pred_index]))
+   # Read in the file by line, load into array, and remove all the newlines
+   with open(file_name) as f:
+      chosen = f.readlines()
+   chosen = [image.strip() for image in chosen] 
+
+   #print(chosen)
+
+   for image in chosen:
+      filepath = os.path.join(DATADIR, image)
+      prediction = model.predict(prepare(filepath))#, verbose=1) # gives back a one hot encoding
+      top_n_predictions(prediction, image, CATEGORIES, n)
+
