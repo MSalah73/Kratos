be6ed554aa6dd4216cb1662b21749769967699e6
YuuLi <liyu_0705@outlook.com>
2019-02-18 20:39:18 -0800

    Update runModel.py

--- runModel.py
+++ runModel.py
@@ -29,8 +29,9 @@ def predict():
     try:
         file = request.files['photo']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], "UploadedPhoto.jpg"))
-	#rm.predict() will return the list of top 5 categories(strings) 
-	result = rm.predict(model,'UploadedPhoto.jpg')
+	# rm.predict() will return the matrix of top 5 categories(strings), 
+	# each row is the top 5 categories of a image
+ 	result = rm.predict(model,'UploadedPhoto.jpg')
 	predictionJson = {"name": "Yu" , "type": "category", "prediction": result[0]}
         #stringPrediction = rm.predict(model,'UploadedPhoto.jpg')
         return jsonify(prediction=predictionJson)
