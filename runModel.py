c2adf092d8c5d547f71e1ef6a20602541636eba2
YuuLi <liyu_0705@outlook.com>
2019-02-18 19:47:09 -0800

    Update runModel.py

--- runModel.py
+++ runModel.py
@@ -12,7 +12,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'jpeg'])
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FLODER
-
+'''
 CATEGORIES = ["Anorak", "Blazer", "Blouse", "Bomber", "Button-Down",
         "Cardigan", "Flannel", "Halter", "Henley", "Hoodie",
         "Jacket", "Jersey", "Parka", "Peacoat", "Poncho",
@@ -23,19 +23,19 @@ CATEGORIES = ["Anorak", "Blazer", "Blouse", "Bomber", "Button-Down",
         "Trunks", "Caftan", "Cape", "Coat", "Coverup",
         "Dress", "Jumpsuit", "Kaftan", "Kimono", "Nightdress",
         "Onesie", "Robe", "Romper", "Shirtdress", "Sundress"]
-
+'''
 model = cm.create_model()
 model.summary()
 model.load_weights('model_weights.h5')
 
-
+'''
 def prepare(file):
     image_string = cv2.imread(file)
     image_resized = cv2.resize(image_string,(300,300))
     image = cv2.cvtColor(image_resized,cv2.COLOR_BGR2RGB)
     image = image/255.0
     return image.reshape(-1,300,300,3)
-
+'''
 @app.route("/")
 def initialAPIPage():
 	return "Connected!!!"
@@ -45,6 +45,7 @@ def predict():
     try:
         file = request.files['photo']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], "UploadedPhoto.jpg"))
+	'''
         prediction = model.predict(prepare('UploadedPhoto.jpg'))
         prediction = np.argsort(prediction)
         prediction = prediction[len(CATEGORIES)-5:]
@@ -52,7 +53,8 @@ def predict():
         temp = []
         for i in prediction:
             temp.append(dp.PROPERTY.CATEGORIES[i])
-        stringPrediction = np.asarray(temp)
+	 '''
+        stringPrediction = rm.predict(model,'UploadedPhoto.jpg')
         return jsonify(prediction=stringPrediction)
     except Exception as e:
         raise e
