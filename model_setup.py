42d5afc78d72e817e6885df0cf97a7bfdde2bf7a
Raymond Emory <remory@babbage.cs.pdx.edu>
2019-02-22 18:22:06 -0800

    using a slower learning rate & tanh

--- model_setup.py
+++ model_setup.py
@@ -18,12 +18,13 @@ def get_model():
     model = tf.keras.Sequential([
             tf.keras.layers.Conv2D(filters=8, kernel_size=5, strides=2, input_shape=(FLAGS.height, FLAGS.width, 3)), #CPU
             #tf.keras.layers.Conv2D(filters=8, kernel_size=2, input_shape=(3, FLAGS.height, FLAGS.width)), #GPU
+            tf.keras.layers.Activation("tanh")
             tf.keras.layers.LeakyReLU(),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Conv2D(filters=16, kernel_size=3),
             tf.keras.layers.LeakyReLU(),
             tf.keras.layers.BatchNormalization(),
-            #tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+            tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
             #tf.keras.layers.Conv2D(filters=64, kernel_size=(2,2), strides=2),
             #tf.keras.layers.LeakyReLU(),
             #tf.keras.layers.BatchNormalization(),
@@ -37,25 +38,25 @@ def get_model():
             #tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(1024),
-            #tf.keras.layers.LeakyReLU(),
-            #tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dense(512, activation=tf.keras.activations.sigmoid),
-            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Activation("tanh")
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dense(512),
+            tf.keras.layers.Activation("tanh")
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(256),
-            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Activation("tanh")
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dense(128, activation=tf.keras.activations.sigmoid),
-            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(128),
+            tf.keras.layers.Activation("tanh")
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dropout(0.35),
+            tf.keras.layers.Dropout(0.05),
             tf.keras.layers.Dense(units=FLAGS.classes, activation=tf.keras.activations.sigmoid),
             tf.keras.layers.Activation("sigmoid")
             ])
 
     model.compile(
-            #optimizer=tf.keras.optimizers.Adam(),
-            optimizer=tf.keras.optimizers.RMSprop(),
+            optimizer=tf.keras.optimizers.Adam(lr=0.0001),
+            #optimizer=tf.keras.optimizers.RMSprop(),
             loss=tf.keras.losses.binary_crossentropy)
             #metrics=['accuracy'])
 
