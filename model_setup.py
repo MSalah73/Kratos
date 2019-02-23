aaa80c60bd37ba3120d61d6368c588413ffafb49
Raymond Emory <remory@babbage.cs.pdx.edu>
2019-02-22 18:23:05 -0800

    using a slower learning rate & tanh

--- model_setup.py
+++ model_setup.py
@@ -18,7 +18,7 @@ def get_model():
     model = tf.keras.Sequential([
             tf.keras.layers.Conv2D(filters=8, kernel_size=5, strides=2, input_shape=(FLAGS.height, FLAGS.width, 3)), #CPU
             #tf.keras.layers.Conv2D(filters=8, kernel_size=2, input_shape=(3, FLAGS.height, FLAGS.width)), #GPU
-            tf.keras.layers.Activation("tanh")
+            tf.keras.layers.Activation("tanh"),
             tf.keras.layers.LeakyReLU(),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Conv2D(filters=16, kernel_size=3),
@@ -38,16 +38,16 @@ def get_model():
             #tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(1024),
-            tf.keras.layers.Activation("tanh")
+            tf.keras.layers.Activation("tanh"),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(512),
-            tf.keras.layers.Activation("tanh")
+            tf.keras.layers.Activation("tanh"),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(256),
-            tf.keras.layers.Activation("tanh")
+            tf.keras.layers.Activation("tanh"),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dense(128),
-            tf.keras.layers.Activation("tanh")
+            tf.keras.layers.Activation("tanh"),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dropout(0.05),
             tf.keras.layers.Dense(units=FLAGS.classes, activation=tf.keras.activations.sigmoid),
